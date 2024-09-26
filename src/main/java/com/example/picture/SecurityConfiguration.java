ebfef3b6df9e37c64aad6e299b5a9f9c4572b0ec
green1490 <zambokrisztian1490@gmail.com>
2024-09-26 20:08:45 +0200

    security

---
+++ src/main/java/com/example/picture/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.example.picture;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    
+    @Bean
+	//enables form Login and HTTP basic auth
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			// restricts urls
+			.authorizeHttpRequests((authorize) -> authorize
+			// any request is authenticated	
+			.anyRequest().authenticated()
+			)
+			.httpBasic(Customizer.withDefaults())
+			.formLogin(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+    @Bean
+	// auth support in memory
+	public UserDetailsService userDetailsService() {
+		@SuppressWarnings("deprecation")
+		UserDetails userDetails = User.withDefaultPasswordEncoder()
+			.username("user")
+			.password("password")
+			.roles("USER")
+			.build();
+
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+}
